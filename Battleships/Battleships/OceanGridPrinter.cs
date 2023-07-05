46ee8dfa3d4f29c881630cf7ed612440621462cc
Félix Díez Arias <fdiez@tunstall.es>
2023-07-05 15:37:46 +0200

    GREEN: OceanGridPrinterTest.should_print_gun_ship

--- Battleships/Battleships/OceanGridPrinter.cs
+++ Battleships/Battleships/OceanGridPrinter.cs
@@ -1,9 +1,42 @@
-﻿namespace Battleships;
+﻿using System.Text;
+
+namespace Battleships;
 
 public class OceanGridPrinter
 {
-    public string Print(List<Ship> ships)
+    public string PrintOceanGrid(List<Ship> ships)
     {
-        throw new NotImplementedException();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.AppendLine();
+        stringBuilder.Append("    | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
+
+        for (int row = 0; row < 10; row++)
+        {
+            stringBuilder.AppendLine();
+            stringBuilder.Append($"   {row}|");
+
+            for (int col = 0; col < 10; col++)
+            {
+                string cellRepresentation = GetCellRepresentation(ships, row, col);
+                stringBuilder.Append($" {cellRepresentation} |");
+            }
+        }
+
+        var result = stringBuilder.ToString();
+
+        return result;
+    }
+
+    private string GetCellRepresentation(List<Ship> ships, int row, int col)
+    {
+        var match = ships.SingleOrDefault(x => x.CoordinatesList.Any(y => y.XPosition == row && y.YPosition == col));
+
+        if (match != null)
+        {
+            return match.Representation;
+        }
+
+        return " ";
     }
 }
--- Battleships/Battleships/Ship.cs
+++ Battleships/Battleships/Ship.cs
@@ -2,8 +2,25 @@
 
 public class Ship
 {
-    public Ship(params Coordinates[] coordinates)
+    private readonly Coordinates[] _coordinatesList;
+
+    public Ship(params Coordinates[] coordinatesList)
     {
-        
+        _coordinatesList = coordinatesList;
+    }
+
+    public Coordinates[] CoordinatesList => _coordinatesList;
+
+    public string Representation
+    {
+        get
+        {
+            if (_coordinatesList.Length == 1)
+            {
+                return "g";
+            }
+
+            return string.Empty;
+        }
     }
 }
